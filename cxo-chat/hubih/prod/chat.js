882375ea2e58c484100acb9c2be7f3fff5380bd1
grayskyesHUB <gary.sykes@hubinternational.com>
2025-06-26 16:57:38 -0400

    Added skills for IH Service, Sales and Travellers

--- cxo-chat/hubih/prod/chat.js
+++ cxo-chat/hubih/prod/chat.js
@@ -7,7 +7,7 @@ parent.cxone('chat', 'onPushUpdate', ['CaseToRoutingQueueAssignmentChanged'], pu
     }
 
     const skillIdTest = pushUpdatePayload.data.routingQueue?.skillId;
-    const hiddenSkillIds = [20899728, 20899729, 23125510, 23125511]; // Add more skill IDs here as needed
+    const hiddenSkillIds = [20899730, 20899731, 20899732]; // Add more skill IDs here as needed
 
     if (hiddenSkillIds.includes(skillIdTest)) {
         console.log(pushUpdatePayload);
